d49d071da944d4c971d362bdee3c72e28094b545
ada0l <andreika.varfolomeev@yandex.ru>
2021-12-07 17:42:43 +1000

    update cors in index.php

--- backend/html/index.php
+++ backend/html/index.php
@@ -5,10 +5,25 @@ require_once __DIR__ . "/../vendor/autoload.php";
 use Src\Core\DatabaseConnector;
 
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Request-Method: OPTIONS, GET, POST");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, X-Custom-Header");
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+  header('Access-Control-Allow-Credentials: true');
+  header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE, PATCH");
+
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+    header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+  exit(0);
+}
 
 session_start();
 
@@ -28,7 +43,7 @@ switch ($uri[1]) {
 }
 
 (new $controller(
-    $dbConnector,
-    $_SERVER["REQUEST_METHOD"],
-    $params
+  $dbConnector,
+  $_SERVER["REQUEST_METHOD"],
+  $params
 ))->processRequest();
