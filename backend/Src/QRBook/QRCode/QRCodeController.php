2c8e4bd0399af4cd113cadcb042b39173fecbe93
ada0l <andreika.varfolomeev@yandex.ru>
2021-12-13 00:38:41 +1000

    QRCodeController.php

--- backend/Src/QRBook/QRCode/QRCodeController.php
+++ backend/Src/QRBook/QRCode/QRCodeController.php
@@ -9,6 +9,17 @@ class QRCodeController extends BaseControllerWithItemModel
     public function __construct($db, $requestMethod, $params)
     {
         parent::__construct($db, $requestMethod, $params, "Src\QRBook\QRCode\QRCodeModel");
+        $this->addMethodValidator("POST", "Src\QRBook\QRCode\CreateQRCodeValidator");
+        $this->addMethodValidator("PUT", "Src\QRBook\QRCode\CreateQRCodeValidator");
     }
 
+    public function beforeCreate($auth, & $input) {
+        if ($input['isURL']) {
+            $timestamp = (new \DateTime())->getTimestamp();
+            $input['uuid'] = md5("${input['title']}${input['text']}${input['user_id']}${timestamp}");
+        } else {
+            $input['uuid'] = null;
+        }
+        unset($input['isURL']);
+    }
 }
